9082b5b3ee0c22632f48445ec4a04898888909cf
Qian LIU <liuqian@ucas.ac.cn>
2018-06-15 16:20:02 +0800

    v1.3
    add G4gen.ini stored feature

--- Electron/sections/Generator/generator.js
+++ Electron/sections/Generator/generator.js
@@ -1,5 +1,6 @@
 const generator = document.getElementById('generator')
 const genOutput = document.getElementById('genOutput')
+const fs = require("fs");
 
 function getMattCode(value){
     code = ""
@@ -53,16 +54,19 @@ generator.addEventListener('click', function () {
     conreg = new RegExp(/c\d/, 'g')
 
     mesg = "<br />output:<br>"
+    outp = ""
     for(mkey in iniData) 
     {
         if(conreg.test(mkey)) {
             mesg = mesg + "________________________________<br><p>[" +iniData[mkey] + "]</p>"
+            outp = outp + "\n[" + iniData[mkey] +"]\n" 
         }
         else if(mkey=="cend") {
             break
         }
         else if(mkey == "NBody") {
-            mesg =  mesg + "<p>" + mkey + " : " + iniData[mkey] + "</p><br />"
+            mesg = mesg + "<p>" + mkey + " : " + iniData[mkey] + "</p><br />"
+            outp = outp + mkey + " : " + iniData[mkey] + "\n"
 
             for(index=1; index<=parseInt(iniData[mkey]); index++) 
             {
@@ -73,19 +77,33 @@ generator.addEventListener('click', function () {
                     if(nkey.indexOf("BodyShap")==0) value = getShapeCode(value)
 
                     mesg =  mesg + "<p>" + nkey+" : " + value + "</p>"
+                    outp =  outp + nkey + " : " + value + "\n"
                 })
-                if(index != parseInt(iniData[mkey]))
+                if(index != parseInt(iniData[mkey])) {
                     mesg = mesg + "<br />"
+                    outp = outp + "\n"
+                }
             }
         }
         else if(mkey.indexOf("GunParticle")==0) {
             value = iniData[mkey]
             value = getGunCode(value)
             mesg =  mesg + "<p>" + mkey+" : " + value + "</p>"
+            outp =  outp + mkey+" : " + value + "\n"
         }
         else {
             mesg =  mesg + "<p>" + mkey+" : " + iniData[mkey] + "</p>"
+            outp =  outp + mkey+" : " + iniData[mkey] + "\n"
         }
     }
     genOutput.innerHTML = mesg
+
+    fs.writeFile("../G4gen2.ini", outp, function (err) {
+        if (err) {
+          alert("Write failed: " + err)
+          return
+        }
+    
+        // alert("test.ini saved.")
+      });
 })
